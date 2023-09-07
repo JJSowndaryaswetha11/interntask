8e4fd30bc98cc50db95ab540056ed8c108a7a9c0
JJsowndaryaswetha11 <nirmalajeyaseeli@gmail.com>
2023-09-07 11:30:23 +0530

    commit changes1

---
+++ Task-4/javascript.js
@@ -0,0 +1,35 @@
+const input=document.getElementById("inputbox");
+const list=document.getElementById("list");
+function addtask(){
+    if(input.value ===''){
+        alert("You must have write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
+        let span= document.createElement("span")
+        span.innerHTML= "\u00d7"
+        li.appendChild(span);
+    }
+    input.value="";
+    saveData();
+}
+list.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName=== "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+    
+},false);
+function saveData(){
+    localStorage.setItem("data",list.innerHTML);
+}
+function showList(){
+    list.innerHTML=localStorage.getItem("data");
+}
+showList();
---
+++ javascript/javascript.js
@@ -0,0 +1,6 @@
+ 
+ 
+ const fooditems=document.querySelectorAll(".food");
+for(const item of fooditems) {
+    console.log(item.innerText);
+} 
